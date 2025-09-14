a08554842b4c10f39446040f8ff6cb98c5f56c73
Andreza Moreira <andrezamor3ira@gmail.com>
2025-09-14 13:08:47 -0300

    Atualização do portfólio

--- js/site.js
+++ js/site.js
@@ -1,35 +1,35 @@
-const imgPetShop = document.querySelector('.btn-modal-PetShop');
-const modalPet = document.querySelector('.PetShop');
-const btnfechar = document.querySelector('.btn-fechar');
+const imgPokeLab = document.querySelector(".btn-modal-PokeLab");
+const modalPoke = document.querySelector(".PokeLab");
+const btnfechar = document.querySelector(".btn-fechar");
 
-const imgJogoMemoria = document.querySelector('.btn-modal-JogoMemoria');
-const modalJogoMemoria = document.querySelector('.JogoMemoria');
-const btnfecharM = document.querySelector('.btn-fechar-Memoria');
+const imgJogoMemoria = document.querySelector(".btn-modal-JogoMemoria");
+const modalJogoMemoria = document.querySelector(".JogoMemoria");
+const btnfecharM = document.querySelector(".btn-fechar-Memoria");
 
-const imgFitaFilmes = document.querySelector('.btn-modal-FitaFilmes');
-const modalFitaFilmes = document.querySelector('.FitaFilmes');
-const btnfecharF = document.querySelector('.btn-fechar-Fita');
+const imgFitaFilmes = document.querySelector(".btn-modal-FitaFilmes");
+const modalFitaFilmes = document.querySelector(".FitaFilmes");
+const btnfecharF = document.querySelector(".btn-fechar-Fita");
 
-imgPetShop.addEventListener('click', () => {
-    modalPet.classList.remove('hidden');
+imgPokeLab.addEventListener("click", () => {
+  modalPoke.classList.remove("hidden");
 });
 
-btnfechar.addEventListener('click', () => {
-    modalPet.classList.add('hidden');
+btnfechar.addEventListener("click", () => {
+  modalPoke.classList.add("hidden");
 });
 
-imgJogoMemoria.addEventListener('click', () => {
-    modalJogoMemoria.classList.remove('hidden');
+imgJogoMemoria.addEventListener("click", () => {
+  modalJogoMemoria.classList.remove("hidden");
 });
 
-btnfecharM.addEventListener('click', () => {
-    modalJogoMemoria.classList.add('hidden');
+btnfecharM.addEventListener("click", () => {
+  modalJogoMemoria.classList.add("hidden");
 });
 
-imgFitaFilmes.addEventListener('click', () => {
-    modalFitaFilmes.classList.remove('hidden');
+imgFitaFilmes.addEventListener("click", () => {
+  modalFitaFilmes.classList.remove("hidden");
 });
 
-btnfecharF.addEventListener('click', () => {
-    modalFitaFilmes.classList.add('hidden');
+btnfecharF.addEventListener("click", () => {
+  modalFitaFilmes.classList.add("hidden");
 });
